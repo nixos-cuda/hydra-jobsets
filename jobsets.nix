ebb599c3929b75a62102021bcb61e92f3983c28f
Gaetan Lepage <gaetan@glepage.com>
2026-01-27 17:59:39 +0100

    jobsets.nix: refactor

--- jobsets.nix
+++ jobsets.nix
@@ -6,70 +6,59 @@ let
     schedulingshares = 1;
     keepnr = 1;
     emailoverride = "";
+    nixexprinput = "jobsets";
   };
+
+  inputs = {
+    jobsets = {
+      type = "git";
+      value = "https://github.com/nixos-cuda/hydra-jobsets";
+    };
+    nixpkgs-unstable = {
+      type = "git";
+      value = "https://github.com/NixOS/nixpkgs.git nixos-unstable-small";
+    };
+    nixpkgs-stable = {
+      type = "git";
+      value = "https://github.com/NixOS/nixpkgs.git nixos-25.11-small";
+    };
+  };
+
   jobsetDir = "./jobsets";
   projects = {
     nixos-cuda = {
       cuda-gpu-checks-unstable = defaults // {
         description = "Run GPU Tests";
-        nixexprinput = "jobsets";
         nixexprpath = "${jobsetDir}/cuda-tests.nix";
         keepnr = 0;
         inputs = {
-          jobsets = {
-            type = "git";
-            value = "https://github.com/nixos-cuda/hydra-jobsets";
-          };
-          nixpkgs = {
-            type = "git";
-            value = "https://github.com/NixOS/nixpkgs.git nixos-unstable-small";
-          };
+          inherit (inputs) jobsets;
+          nixpkgs = inputs.nixpkgs-unstable;
         };
       };
       cuda-gpu-checks-stable = defaults // {
         description = "Run GPU Tests [STABLE]";
-        nixexprinput = "jobsets";
         nixexprpath = "${jobsetDir}/cuda-tests.nix";
         keepnr = 0;
         inputs = {
-          jobsets = {
-            type = "git";
-            value = "https://github.com/nixos-cuda/hydra-jobsets";
-          };
-          nixpkgs = {
-            type = "git";
-            value = "https://github.com/NixOS/nixpkgs.git nixos-25.11-small";
-          };
+          inherit (inputs) jobsets;
+          nixpkgs = inputs.nixpkgs-stable;
         };
       };
       cuda-packages-unstable = defaults // {
         description = "All (?) nixpkgs cudaSupport-sensitive packages";
-        nixexprinput = "jobsets";
         nixexprpath = "${jobsetDir}/cuda-packages.nix";
         inputs = {
-          nixpkgs = {
-            type = "git";
-            value = "https://github.com/NixOS/nixpkgs.git nixos-unstable-small";
-          };
-          jobsets = {
-            type = "git";
-            value = "https://github.com/nixos-cuda/hydra-jobsets";
-          };
+          inherit (inputs) jobsets;
+          nixpkgs = inputs.nixpkgs-unstable;
         };
       };
       cuda-packages-stable = defaults // {
         description = "All (?) nixpkgs cudaSupport-sensitive packages [STABLE]";
-        nixexprinput = "jobsets";
         nixexprpath = "${jobsetDir}/cuda-packages.nix";
         inputs = {
-          nixpkgs = {
-            value = "https://github.com/NixOS/nixpkgs.git nixos-25.11-small";
-            type = "git";
-          };
-          jobsets = {
-            type = "git";
-            value = "https://github.com/nixos-cuda/hydra-jobsets";
-          };
+          inherit (inputs) jobsets;
+          nixpkgs = inputs.nixpkgs-stable;
         };
       };
     };
