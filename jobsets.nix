8a74daf1348bdbfdf761b9e4149c33c6244ba074
Gaetan Lepage <gaetan@glepage.com>
2026-02-06 12:33:47 +0100

    jobsets.nix: cosmetic refactor

--- jobsets.nix
+++ jobsets.nix
@@ -9,55 +9,62 @@ let
     nixexprinput = "jobsets";
   };
 
-  inputs = {
-    jobsets = {
-      type = "git";
-      value = "https://github.com/nixos-cuda/hydra-jobsets";
-    };
-    nixpkgs-unstable = {
-      type = "git";
-      value = "https://github.com/NixOS/nixpkgs.git nixos-unstable-small";
-    };
-    "nixpkgs-25.11" = {
-      type = "git";
-      value = "https://github.com/NixOS/nixpkgs.git nixos-25.11-small";
-    };
+  nixpkgsChannelNames = {
+    unstable = "nixos-unstable-small";
+    "25.11" = "nixos-25.11-small";
   };
 
-  jobsetDir = "./jobsets";
+  mkJobset =
+    {
+      nixpkgsRelease,
+      description,
+      definitionFile,
+    }:
+    let
+      nixpkgsChannelName = nixpkgsChannelNames.${nixpkgsRelease};
+    in
+    defaults
+    // {
+      description = "${description} [${nixpkgsChannelName}]";
+      nixexprpath = "./jobsets/${definitionFile}";
+
+      inputs = {
+        jobsets = {
+          type = "git";
+          value = "https://github.com/nixos-cuda/hydra-jobsets";
+        };
+
+        nixpkgs = {
+          type = "git";
+          value = "https://github.com/NixOS/nixpkgs.git ${nixpkgsChannelName}";
+        };
+      };
+    };
+
   projects = {
-    nixos-cuda = {
-      cuda-gpu-checks-unstable = defaults // {
-        description = "Run GPU Tests [nixos-unstable-small]";
-        nixexprpath = "${jobsetDir}/cuda-tests.nix";
-        inputs = {
-          inherit (inputs) jobsets;
-          nixpkgs = inputs.nixpkgs-unstable;
-        };
+    nixos-cuda = builtins.mapAttrs (_: mkJobset) {
+      # GPU CHECKS
+      cuda-gpu-checks-unstable = {
+        description = "Run GPU Tests";
+        definitionFile = "cuda-tests.nix";
+        nixpkgsRelease = "unstable";
       };
-      "cuda-gpu-checks-25.11" = defaults // {
-        description = "Run GPU Tests [nixos-25.11-small]";
-        nixexprpath = "${jobsetDir}/cuda-tests.nix";
-        inputs = {
-          inherit (inputs) jobsets;
-          nixpkgs = inputs."nixpkgs-25.11";
-        };
+      "cuda-gpu-checks-25.11" = {
+        description = "Run GPU Tests";
+        definitionFile = "cuda-tests.nix";
+        nixpkgsRelease = "25.11";
       };
-      cuda-packages-unstable = defaults // {
-        description = "All (?) nixpkgs cudaSupport-sensitive packages [nixos-unstable-small]";
-        nixexprpath = "${jobsetDir}/cuda-packages.nix";
-        inputs = {
-          inherit (inputs) jobsets;
-          nixpkgs = inputs.nixpkgs-unstable;
-        };
+
+      # PACKAGES
+      cuda-packages-unstable = {
+        description = "All nixpkgs cudaSupport-sensitive packages";
+        definitionFile = "cuda-packages.nix";
+        nixpkgsRelease = "unstable";
       };
-      "cuda-packages-25.11" = defaults // {
-        description = "All (?) nixpkgs cudaSupport-sensitive packages [nixos-25.11-small]";
-        nixexprpath = "${jobsetDir}/cuda-packages.nix";
-        inputs = {
-          inherit (inputs) jobsets;
-          nixpkgs = inputs."nixpkgs-25.11";
-        };
+      "cuda-packages-25.11" = {
+        description = "All nixpkgs cudaSupport-sensitive packages";
+        definitionFile = "cuda-packages.nix";
+        nixpkgsRelease = "25.11";
       };
     };
   };
